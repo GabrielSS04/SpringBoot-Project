7426a42afa8c84d73fb607fc58bec24c4ebf1f4d
stainhaus <rafagabisouza6@gmail.com>
2023-11-09 00:03:36 -0300

    commit
    Upload do Projeto em fase de produção

---
+++ main/java/com/fag/SpringBoot/Jogada.java
@@ -0,0 +1,23 @@
+package com.fag.SpringBoot;
+
+public class Jogada {
+
+    private String jogador;
+    private String computador;
+
+    public String getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(String jogador) {
+        this.jogador = jogador;
+    }
+
+    public String getComputador() {
+        return computador;
+    }
+
+    public void setComputador(String computador) {
+        this.computador = computador;
+    }
+}
